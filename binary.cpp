7f8a43f81c7dc93e776b4f6abec795ae3d79528e
JonathanLoza <lozajnt@gmail.com>
2018-10-30 23:57:13 -0500

    binary array terminado

--- binary.cpp
+++ binary.cpp
@@ -1,34 +1,74 @@
 #ifndef BOOLEAN_H
 #define BOOLEAN_H
+#include <iostream>
+using namespace std;
 
 class boolean {
     private:
         char* array;
         int size;
-    
+
     public:
         boolean(int size) {
-            // TODO
+          this->size=size;
+            array= new char[(size/8)+1];
+            for(int i=0; i<(size/8)+1; i++){
+              array[i]=0;
+            }
         }
-        
+
         void bitOn(int position) {
-            // TODO
+            if(position>size){
+              cout<<"fuera de rango"<<endl;
+            }
+            else{
+              int n=position/8;
+              int x=position%8;
+              char mask=1;
+              mask=(mask << x);
+              cout<<(int)array[n]<<endl;
+              array[n]|=(mask);
+              cout<<(int)array[n]<<endl;
+            }
         }
-             
+
         void bitOff(int position) {
-            // TODO
+            if(position>size){
+              cout<<"fuera de rango"<<endl;
+            }
+            else{
+              int n=position/8;
+              int x=position%8;
+              char mask=1;
+              mask=(mask << x);
+              cout<<(int)array[n]<<endl;
+              array[n]&= ~(mask);
+              cout<<(int)array[n]<<endl;
+            }
         }
-             
+
         bool operator[](int position) {
-            // TODO
+          if(position>size){
+            cout<<"fuera de rango"<<endl;
+            throw "error";
+          }
+          else{
+            int n=position/8;
+            int x=position%8;
+            char mask=array[n];
+            bool bit = (mask >> x)& 1U;
+            return bit;
+          }
         }
-    
-        void clear() {    
-            // TODO
+
+        void clear() {
+            for(int i=0; i<(size/8)+1;i++){
+              array[i]=0;
+            }
         }
-             
+
         ~boolean() {
-            // TODO
+          delete array;
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include "binary.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
+    cout << "Binary Array Practice" << endl;
     cout << "===========================================================" << endl << endl;
+    /*char *array=new char[(1/8)+1];
+    char mask=1;
+    array[0] |= (mask);
+    unsigned long bit = (array[0] >> 1)& 1U;
+    cout<<bit;*/
+    boolean boleanos(8);
+    boleanos.bitOn(2);
+    bool x=boleanos[2];
+    cout<<x<<endl;
+    boleanos.bitOff(2);
 
-    system("read");
+
+    //system("read");
     return EXIT_SUCCESS;
 }
-
